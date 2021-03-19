9f3044c57fe8688d53942e08410a063e21f87637
philhow <phil.w.howard@gmail.com>
2021-03-19 16:50:23 -0700

    Imported Bulkhead and Geometry classes

---
+++ AVSHull/Bulkhead.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Media3D;
+
+namespace AVSHull
+{
+    // Class representing a bulkhead: A closed polygon that represents a slice through a hull.
+    public class Bulkhead : INotifyPropertyChanged
+    {
+        private const double NEAR_ZERO = 0.02;
+
+        public enum BulkheadType { BOW, VERTICAL, TRANSOM };
+        public BulkheadType type { get; set; }
+
+        private double m_transomAngle;
+        public double TransomAngle {  get { return m_transomAngle; } }
+
+        private Point3DCollection m_points;
+        public Point3DCollection Points {  get { return m_points; } }
+
+        public Bulkhead()
+        {
+            m_points = new Point3DCollection();
+        }
+
+        public void LoadFromHullFile(StreamReader file, int numChines, BulkheadType type)
+        {
+            this.type = type;
+            m_points = new Point3DCollection();
+
+            string line;
+            Point3DCollection temp_points = new Point3DCollection();
+            for (int chine = 0; chine < numChines; chine++)
+            {
+                Point3D point = new Point3D();
+                double value;
+                line = file.ReadLine();
+                if (!double.TryParse(line, out value)) throw new Exception("Unable to read bulkhead X value");
+                point.X = value;
+
+                line = file.ReadLine();
+                if (!double.TryParse(line, out value)) throw new Exception("Unable to read bulkhead Y value");
+                point.Y = value;
+
+                line = file.ReadLine();
+                if (!double.TryParse(line, out value)) throw new Exception("Unable to read bulkhead Z value");
+                point.Z = value;
+
+                temp_points.Add(point);
+            }
+
+
+            if (Math.Abs(m_points[0].X) < NEAR_ZERO)
+            {
+                // Bottom is on center-line
+                for (int ii= temp_points.Count - 1; ii>0; ii--)
+                {
+                    m_points.Add(temp_points[ii]);
+                }
+
+                // Force center to zero
+                m_points.Add(new Point3D(0, temp_points[0].Y, temp_points[0].Z));
+                
+                for (int ii=1; ii<temp_points.Count; ii++)
+                {
+                    m_points.Add(temp_points[ii]);
+                }
+            }
+            else
+            {
+                // flat floor: bottom is not on center-line
+                for (int ii = temp_points.Count - 1; ii >= 0; ii--)
+                {
+                    m_points.Add(temp_points[ii]);
+                }
+
+                for (int ii = 1; ii < temp_points.Count; ii++)
+                {
+                    m_points.Add(temp_points[ii]);
+                }
+            }
+
+            ComputeAngle();
+            StraightenBulkhead();
+
+            Notify("Bulkhead");
+        }
+
+        protected void ComputeAngle()
+        {
+            m_transomAngle = 0;
+            if (type == BulkheadType.TRANSOM)
+            {
+                double delta, max_delta;
+                int max_index = 1;
+
+                // find greatest delta_z
+                max_delta = 0;
+                for (int ii = 1; ii < m_points.Count; ii++)
+                {
+                    delta = Math.Abs(m_points[ii - 1].Z - m_points[ii].Z);
+                    if (delta > max_delta)
+                    {
+                        delta = max_delta;
+                        max_index = ii;
+                    }
+                }
+
+                double delta_y = m_points[0].Y - m_points[max_index].Y;
+                double delta_z = m_points[0].Z - m_points[max_index].Z;
+
+                if (delta_z == 0)
+                    type = BulkheadType.VERTICAL;
+                else
+                    m_transomAngle = Math.Atan2(delta_y, delta_z);
+            }
+        }
+
+        protected void StraightenBulkhead()
+        {
+            for (int chine = 0; chine < m_points.Count; chine++)
+            {
+                // FIXTHIS: uncomment after UpdatePoint works with TRANSOM points. UpdatePoint(chine, 0, 0, 0);
+            }
+        }
+
+        //******************************************
+        // INotifyPropertyChanged implementation
+        public event PropertyChangedEventHandler PropertyChanged;
+        void Notify(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+
+    //**********************************************
+    public class SerializableBulkhead
+    {
+        public double transom_angle;
+        public Bulkhead.BulkheadType bulkheadType;
+        public Point3DCollection points;
+
+        public SerializableBulkhead()
+        {
+            points = new Point3DCollection();
+        }
+
+        public SerializableBulkhead(Bulkhead bulkhead)
+        {
+            transom_angle = bulkhead.TransomAngle;
+            bulkheadType = bulkhead.type;
+            points = bulkhead.Points.Clone();
+        }
+    }
+}
---
+++ AVSHull/Matrix.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Media3D;
+
+namespace AVSHull
+{
+    class Matrix
+    {
+        public static void Multiply(double[,] left, double[,] right, out double[,] returnMatrix)
+        {
+            double[,] result;       // temp array so we can compute in place
+            result = new double[left.GetLength(0), left.GetLength(1)];
+
+            for (int arow = 0; arow < left.GetLength(0); arow++)
+            {
+                for (int bcol = 0; bcol < right.GetLength(1); bcol++)
+                {
+                    result[arow, bcol] = 0;
+                    for (int acol = 0; acol < left.GetLength(1); acol++)
+                    {
+                        result[arow, bcol] += left[arow, acol] * right[acol, bcol];
+                    }
+                }
+            }
+
+            returnMatrix = result;
+        }
+
+        public static void Multiply(Point3DCollection left, double[,] right, out Point3DCollection returnMatrix)
+        {
+            Point3DCollection result = new Point3DCollection(left.Count);   // temp array so we can compute in place
+
+            for (int ii = 0; ii < left.Count; ii++)
+            {
+                Point3D point = new Point3D();
+                point.X = left[ii].X * right[0, 0] + left[ii].Y * right[1, 0] + left[ii].Z * right[2, 0];
+                point.Y = left[ii].X * right[0, 1] + left[ii].Y * right[1, 1] + left[ii].Z * right[2, 1];
+                point.Z = left[ii].X * right[0, 2] + left[ii].Y * right[1, 2] + left[ii].Z * right[2, 2];
+
+                result.Add(point);
+            }
+
+            returnMatrix = result;
+        }
+        public static void Multiply(List<Rect> left, double[,] right, out List<Rect> returnMatrix)
+        {
+            List<Rect> result = new List<Rect>(left.Count);   // temp array so we can compute in place
+
+            foreach (Rect rect in left)
+            {
+                Point point = new Point();
+                point.X = rect.Location.X * right[0, 0] + rect.Location.Y * right[1, 0];
+                point.Y = rect.Location.X * right[0, 1] + rect.Location.Y * right[1, 1];
+
+                result.Add(new Rect(point, rect.Size));
+            }
+
+            returnMatrix = result;
+        }
+
+        public static void Multiply(PointCollection left, double[,] right, out PointCollection returnMatrix)
+        {
+            PointCollection result = new PointCollection(left.Count);   // temp array so we can compute in place
+
+            for (int ii = 0; ii < left.Count; ii++)
+            {
+                Point point = new Point();
+                point.X = left[ii].X * right[0, 0] + left[ii].Y * right[1, 0];
+                point.Y = left[ii].X * right[0, 1] + left[ii].Y * right[1, 1];
+
+                result.Add(point);
+            }
+
+            returnMatrix = result;
+        }
+    }
+}
