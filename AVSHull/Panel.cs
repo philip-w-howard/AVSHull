69d364ed4bcfef67f2c320ed8d5ed4fd9abdf369
philhow <phil.w.howard@gmail.com>
2021-09-03 17:27:04 -0700

    Use smallest angle, not greatest X

--- AVSHull/Panel.cs
+++ AVSHull/Panel.cs
@@ -124,11 +124,27 @@ namespace AVSHull
                     Debug.WriteLine("Intersection a {0}: ({1:F2}) r: {2:F2}  ({3:F2}) r: {4:F2} ::= ({5:F2}) ({6:F2})",
                         m_panelPoints.Count, m_panelPoints[m_panelPoints.Count - 1], r1, edge2[edge2.Count - 1], r2, intersection_a1, intersection_a2);
 
-                    if (intersection_a1.X >= intersection_a2.X)
-                        m_panelPoints.Add(intersection_a1);
+                    if (ii == 0)
+                    {
+                        if (intersection_a1.X >= intersection_a2.X)
+                            m_panelPoints.Add(intersection_a1);
+                        else
+                            m_panelPoints.Add(intersection_a2);
+                    }
                     else
-                        m_panelPoints.Add(intersection_a2);
+                    {
+                        Vector v_1 = m_panelPoints[m_panelPoints.Count - 1] - m_panelPoints[m_panelPoints.Count - 2];
+                        Vector v_1a = intersection_a1 - m_panelPoints[m_panelPoints.Count - 1];
+                        Vector v_1b = intersection_a2 - m_panelPoints[m_panelPoints.Count - 1];
 
+                        double a1 = Math.Abs(Vector.AngleBetween(v_1, v_1a));
+                        double a2 = Math.Abs(Vector.AngleBetween(v_1, v_1b));
+
+                        if (a1 < a2)
+                            m_panelPoints.Add(intersection_a1);
+                        else
+                            m_panelPoints.Add(intersection_a2);
+                    }
                     // advance edge2 by one point
                     r1 = (chine2[ii] - chine2[ii + 1]).Length;
                     r2 = (chine1[ii + 1] - chine2[ii + 1]).Length;
@@ -136,10 +152,27 @@ namespace AVSHull
                     Debug.WriteLine("Intersection b {0}: ({1:F2}) r: {2:F2}  ({3:F2}) r: {4:F2} ::= ({5:F2}) ({6:F2})",
                         m_panelPoints.Count, edge2[edge2.Count - 1], r1, m_panelPoints[m_panelPoints.Count - 1], r2, intersection_b1, intersection_b2);
 
-                    if (intersection_b1.X >= intersection_b2.X)
-                        edge2.Add(intersection_b1);
+                    if (ii == 0)
+                    {
+                        if (intersection_b1.X >= intersection_b2.X)
+                            edge2.Add(intersection_b1);
+                        else
+                            edge2.Add(intersection_b2);
+                    }
                     else
-                        edge2.Add(intersection_b2);
+                    {
+                        Vector v_2 = edge2[edge2.Count - 1] - edge2[edge2.Count - 2];
+                        Vector v_2a = intersection_b1 - edge2[edge2.Count - 1];
+                        Vector v_2b = intersection_b2 - edge2[edge2.Count - 1];
+
+                        double b1 = Math.Abs(Vector.AngleBetween(v_2, v_2a));
+                        double b2 = Math.Abs(Vector.AngleBetween(v_2, v_2b));
+
+                        if (b1 < b2)
+                            edge2.Add(intersection_b1);
+                        else
+                            edge2.Add(intersection_b2);
+                    }
                 }
             }
 
